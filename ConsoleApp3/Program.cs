69ccb3b3d49026c516bec7a472420118691240de
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-12-30 19:35:14 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,31 +8,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Database database = new();
-
+            
             database.ShowItems();
         }
     }
 
-    class Database 
+    class Database
     {
         private List<Player> DataPlayers = new();
 
         public Database()
         {
-            Console.Write("Введите номер: ");
-            DataPlayers.Add(new Player(Convert.ToInt32(Console.ReadLine())));
-            Console.Write("Введите уровень: ");
-            DataPlayers.Add(new Player(Convert.ToInt32(Console.ReadLine())));
-            Console.Write("Введите никнейм: ");
-            DataPlayers.Add(new Player(Console.ReadLine()));
+            //Console.Write("Номер: ");
+            //Console.WriteLine("\nНик: ");
+            //Console.WriteLine("\nУровень: ");
+            //DataPlayers.Add(new Player(Convert.ToInt32(Console.ReadLine()), Console.ReadLine(), Convert.ToInt32(Console.ReadLine())));
+            //Console.Write("Введите номер: ");
+            //DataPlayers.Add(new Player(Convert.ToInt32(Console.ReadLine())));
+            //Console.Write("Введите уровень: ");
+            //DataPlayers.Add(new Player(Convert.ToInt32(Console.ReadLine())));
+            //Console.Write("Введите никнейм: ");
+            //DataPlayers.Add(new Player(Console.ReadLine()));
+            DataPlayers.Add(new Player(Convert.ToInt32(Console.ReadLine()), Console.ReadLine(), Convert.ToInt32(Console.ReadLine())));
+            
+
         }
 
         public void ShowItems()
         {
-            for (int i = 1; i < DataPlayers.Count; i++)
+            for(int i = 0; i < DataPlayers.Count; i++)
             {
-               Console.Write("Уникальный номер: " + DataPlayers[i].UniqueNumber + "Уровень: " + DataPlayers[i].PlayerLevel
-                + "Никнейм: " + DataPlayers[i].NickName);
+                Console.Write("Уникальный номер: " + DataPlayers[i].UniqueNumber + "Уровень: " + DataPlayers[i].PlayerLevel
+                 + "Никнейм: " + DataPlayers[i].NickName);
             }
         }
     }
@@ -43,16 +50,17 @@ namespace ConsoleApp1
 
         public string NickName { get; private set; }
 
-        public int UniqueNumber { get; private set; }  
+        public int UniqueNumber { get; private set; }
 
-        public Player(int gamerLevel)
+        public Player(int playerLevel)
         {
-            PlayerLevel = gamerLevel;   
+            PlayerLevel = playerLevel;     
         }
 
-        public Player(string moniker)
+        public Player(int playerLevel, string nickName, int uniqueNumber) : this(playerLevel)
         {
-            NickName = moniker; 
+            NickName = nickName;
+            UniqueNumber = uniqueNumber;
         }
     }
 }
