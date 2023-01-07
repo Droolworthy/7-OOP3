ef51855bd8dd209645d84586e637be3d0b4fa3ca
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-01-07 15:42:05 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,8 +6,19 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {        
-            Database database = new();
+        {
+            string enterNumber = "Уникальный номер: ";
+            string enterLevel = "\nУровень: ";
+            string enterNickName = "\nНикнейм: ";
+
+            Console.Write(enterNumber);
+            int individualNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write(enterNickName);
+            string moniker = Console.ReadLine();           
+            Console.Write(enterLevel);
+            int gamerLevel = Convert.ToInt32(Console.ReadLine());
+
+            Database database = new(enterNumber, individualNumber, enterNickName, moniker, enterLevel, gamerLevel);
 
             database.ShowItems();
         }
@@ -17,16 +28,16 @@ namespace ConsoleApp1
     {
         private List<Player> DataPlayers = new();
 
-        public Database()
+        public Database(string enterNumber, int individualNumber, string enterNickName, string moniker, string enterLevel, int gamerLevel)
         {
-            DataPlayers.Add(new Player(Convert.ToInt32(Console.ReadLine()), Console.ReadLine(), Convert.ToInt32(Console.ReadLine())));
+           DataPlayers.Add(new Player(enterNumber, Convert.ToInt32(Console.ReadLine()), enterNickName, moniker, enterLevel, gamerLevel)); 
         }
 
         public void ShowItems()
         {
             for (int i = 0; i < DataPlayers.Count; i++)
             {
-                Console.Write("Уникальный номер: " + DataPlayers[i].UniqueNumber + "\nУровень: " + DataPlayers[i].PlayerLevel
+                Console.Write("\nУникальный номер: " + DataPlayers[i].UniqueNumber + "\nУровень: " + DataPlayers[i].PlayerLevel
                  + "\nНикнейм: " + DataPlayers[i].NickName);
             }
         }
@@ -34,17 +45,27 @@ namespace ConsoleApp1
 
     class Player
     {
+        public string EnterNumber;
+
+        public string EnterLevel;
+
+        public string EnterNickName;
+
         public int PlayerLevel { get; private set; }
 
-        public string NickName { get; private set; }        
+        public string NickName { get; private set; }
 
         public int UniqueNumber { get; private set; }
 
-        public Player(int individualNumber, string moniker, int gamerLevel)
+        public Player(string enterNumber, int individualNumber, string enterNickName, string moniker, string enterLevel, int gamerLevel)
         {
+            EnterNumber = enterNumber;
+            EnterNickName = enterNickName;
+            EnterLevel = enterLevel;
+            EnterNumber = enterNumber;
             UniqueNumber = individualNumber;
             PlayerLevel = gamerLevel;
-            NickName = moniker;
+            NickName = moniker;            
         }
     }
 }
